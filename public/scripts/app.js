7b6507042a25dd64536eb95a397b4ac08021bee3
arshdeep1110 <arshdeepsingh20212022@gmail.com>
2022-06-11 21:11:06 -0400

    Initial Assignment -1

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+// IIFE -- Immediately Invoked Function Expresion
+(function(){
+
+    function start()
+    {
+        console.log("App started...");
+    }
+
+    window.addEventListener("load", start);
+
+})()
